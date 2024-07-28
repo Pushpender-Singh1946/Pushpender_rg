ffdfb5045cad5f063d70f1635488e06af88ea68b
Pushpender Singh <167241030+Pushpender-Singh1946@users.noreply.github.com>
2024-07-28 14:51:44 +0530

    QA

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 
 
 
-resource "azurerm_resource_group" "dev1" {
-  name     = "dev1"
+resource "azurerm_resource_group" "QA1" {
+  name     = "QA1"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "dev2" {
-  name     = "dev2"
+resource "azurerm_resource_group" "QA2" {
+  name     = "QA2"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "dev3" {
-  name     = "dev3"
+resource "azurerm_resource_group" "QA3" {
+  name     = "QA3"
   location = "West Europe"
 }
