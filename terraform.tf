c20ea2eb6952441cb551e0a69ae6171620cff2cd
Pushpender Singh <167241030+Pushpender-Singh1946@users.noreply.github.com>
2024-07-28 14:45:30 +0530

    multiple rg added

--- terraform.tf
+++ terraform.tf
@@ -6,3 +6,24 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+
+resource "azurerm_resource_group" "example1" {
+  name     = "example1"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example3" {
+  name     = "example3"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example4" {
+  name     = "example4"
+  location = "West Europe"
+}
+
