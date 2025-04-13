d7aa442b37ff4270ab6fff915b50200334521a06
deepz0707 <167241030+Pushpender-Singh1946@users.noreply.github.com>
2025-04-13 15:11:01 +0530

    main.tf added

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "newpayararg" {
+  name = "newpayararg"
+    location = "East US"
+}
+
+resource "azurerm_resource_group" "newpayararg1" {
+  name = "newpayararg1"
+    location = "East US"
+}
